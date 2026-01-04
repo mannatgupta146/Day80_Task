4ab875ea6da8907fa94e3aa28d12618662bbb155
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-04 13:27:25 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const [name, setName] = useState('')
+  useEffect(()=>{
+    console.log("hi")
+  })
+
   return (
     <div>
-      
+      <input 
+      type="text" 
+      placeholder='enter name'
+      value={name}
+      onChange={(e)=>{
+        setName(e.target.value)
+      }}
+      />
+
+      <h1>{counter}</h1>
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>
+        Increment Counter
+      </button>
     </div>
   )
 }
