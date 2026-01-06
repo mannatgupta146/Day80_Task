a78753fa86782f2b283eb71208c3efaf2dba4e68
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-06 23:34:26 +0530

    NavLink use

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between p-2'>
       <h1>navbar</h1>
       <div className='flex gap-4 text-black p-2'>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
       </div>
     </div>
   )
