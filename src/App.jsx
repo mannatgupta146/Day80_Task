acb64799520811413325835aa3cdbabe3c68dfc8
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-09 23:52:02 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './pages/Navbar'
 
 const App = () => {
+  const [theme, setTheme] = useState('light') 
+
   return (
     <div>
-      <Navbar/>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/about/:id' element={<About/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/about/:id' element={<About />} />
       </Routes>
     </div>
   )
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ theme, setTheme }) => {
+
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <div className='flex justify-center items-center gap-4 p-2'>
-      <NavLink to="/" style={({ isActive }) => ({
-                    color: isActive ? "red" : "black",
-                    fontWeight: isActive ? "bold" : "normal",
-                    textDecoration: "none",
-                    marginRight: "15px",
-                })}>Home</NavLink>
-        <NavLink to="/about" style={({ isActive }) => ({
-                    color: isActive ? "red" : "black",
-                    fontWeight: isActive ? "bold" : "normal",
-                    textDecoration: "none",
-                    marginRight: "15px",
-                })}>About</NavLink>
+      <h1>theme is {theme}</h1>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          setTheme(inputValue) 
+          setInputValue('') 
+        }}
+      >
+        <input
+          type="text"
+          placeholder="enter color"
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)} 
+        />
+
+        <button type="submit">submit</button>
+      </form>
+
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </div>
   )
 }
